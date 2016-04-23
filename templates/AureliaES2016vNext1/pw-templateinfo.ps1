0fff4e9cdcac229a9301816c18c9b584787e8b73
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-04-23 12:58:02 -0700

    improving template create by adding includes

--- templates/AureliaES2016vNext1/pw-templateinfo.ps1
+++ templates/AureliaES2016vNext1/pw-templateinfo.ps1
@@ -8,31 +8,25 @@ $templateInfo = New-Object -TypeName psobject -Property @{
     Type = 'ProjectTemplate'
     DefaultProjectName = 'AngularSpaApplication'
     AfterInstall = {
-        Start-Sleep -Seconds 5
         Update-VisualStuidoProjects -slnRoot ($SolutionRoot)
         $projdest = $properties['FinalDestPath']
+        $filetoextract = (Join-Path $projdest 'n-modules.7z')
 
         try{
             Push-Location
-            if(-not ([string]::IsNullOrEmpty($projdest)) -and (test-path $projdest) ){
-                Set-Location $projdest
 
-                # $7zippath = 'C:\temp\pecan-waffle\7z1514-extra\7za.exe'
+            # see if the file has already been extracted
+            $extractdest = ([System.IO.DirectoryInfo](Join-Path $env:LOCALAPPDATA 'pecan-waffle\aur2016\nmv1')).FullName
+
+            if(-not (Test-Path $extractdest)){
+                New-Item -Path $extractdest -ItemType Directory
+
+                Set-Location $extractdest
+
                 $7zippath = (Get-7zipExe)
-                $filetoextract = (Join-Path $projdest 'n-modules.7z')
-
-                if(-not [string]::IsNullOrWhiteSpace($logfilepath)){
-                $logmsg = @'
- pwd=[{0}]
- projdest=[{1}]
- filetoextract=[{2}]
-'@
-
-                [System.IO.File]::AppendAllText($logfilepath, ($logmsg -f $pwd,$projdest,$filetoextract))
-                }
 
                 if( -not ([string]::IsNullOrWhiteSpace($filetoextract)) -and (Test-Path $filetoextract) ){
-                    $cmdargs = @('-bd','x','n-modules.7z',"-wnode_modules")
+                    $cmdargs = @('-bd','x',$filetoextract,('-w{0}' -f $extractdest))
 
                     $cmdargsstr = "`r`n{0} {1}" -f $7zippath,($cmdargs -join ' ')
 
@@ -41,16 +35,20 @@ $templateInfo = New-Object -TypeName psobject -Property @{
                     }
 
                     Invoke-CommandString -command $7zippath -commandArgs $cmdargs -ignoreErrors $true
-                    Remove-Item $filetoextract
                 }
                 else{
                     throw ('Did not find node modules zip at [{0}]' -f $filetoextract)
                 }
-                <# 
-                call npm install - this took about 10 min to complete
-                #>
-                # 'calling npm install' | Write-Output
-                # Invoke-CommandString -command 'npm' -commandArgs 'install'
+            }
+
+            if(-not ([string]::IsNullOrEmpty($projdest)) -and (test-path $projdest) ){
+                if(-not (Test-Path $extractdest)){
+                    throw ('node modules content folder not found at [{0}]' -f $extractdest)
+                }
+
+                $nmdest = ([System.IO.DirectoryInfo](Join-Path $projdest 'node_modules')).FullName
+                Copy-ItemRobocopy -sourcePath "$extractdest\node_modules" -destPath $nmdest -recurse
+
             }
             else{
                 'destPath is empty' | Write-Output
@@ -61,21 +59,25 @@ $templateInfo = New-Object -TypeName psobject -Property @{
             }
         }
         finally{
+            if(Test-Path $filetoextract){
+                Remove-Item $filetoextract
+            }
             Pop-Location
         }        
     }
 }
 
 $templateInfo | replace (
-    ('AureliaES2016vNext1', {"$ProjectName"}, {"$DefaultProjectName"}<#, $null, @('*.xproj'), @('node_modules')#>),
-    ('4aafd9a8-008c-4adf-8089-90c6ed4b96e3', {"$ProjectId"}, {[System.Guid]::NewGuid()}<#,$null, @('*.xproj'), @('node_modules')#>)
+    ('AureliaES2016vNext1', {"$ProjectName"}, {"$DefaultProjectName"},@('.json';'.cs';'.cshtml')),
+    ('4aafd9a8-008c-4adf-8089-90c6ed4b96e3', {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj"))
 )
+
 
 # when the template is run any filename with the given string will be updated
 $templateInfo | update-filename (
-    ,('AureliaES2016vNext1', {"$ProjectName"}<#, $null, @('AureliaES2016vNext1'), @('node_modules')#>)
-    # ,('EmptyProject', {"$ProjectName"},$null,'EmptyProject.xproj')
+    ,('AureliaES2016vNext1', {"$ProjectName"},$null,@('*.*proj'))
 )
+
 # excludes files from the template
 $templateInfo | exclude-file 'pw-*.*','*.user','*.suo','*.userosscache','project.lock.json','*.vs*scc'
 # excludes folders from the template
@@ -83,3 +85,13 @@ $templateInfo | exclude-folder '.vs','artifacts','packages','bin','obj','node_mo
 
 # This will register the template with pecan-waffle
 Set-TemplateInfo -templateInfo $templateInfo
+
+<#
+To get the list of includes for AureliaES2016vNext1 replace item above
+Get-ChildItem NodeServices\templates\AureliaES2016vNext1 * -Recurse -File|select-string 'skeleton_navigation_es2016_vs' -SimpleMatch|Select-Object -ExpandProperty path -Unique|% { Get-Item $_ | Select-Object -ExpandProperty extension}|Select-Object -Unique|%{ Write-Host "'$_';" -NoNewline }
+
+
+To get the list of includes for guid replace above
+Get-ChildItem NodeServices\templates\AureliaES2016vNext1 *.*proj -Recurse -File|Select-Object -ExpandProperty fullname -Unique|% { ([xml](Get-Content $_)).Project.PropertyGroup.ProjectGuid|Select-Object -Unique|%{ '('{0}', {{"$ProjectId"}}, {{[System.Guid]::NewGuid()}},@("*.*proj")),' -f $_ }}
+('4aafd9a8-008c-4adf-8089-90c6ed4b96e3', {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+#>
